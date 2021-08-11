c684f611c0a9e91728f0ee22ba1d622583f09629
JoelBolwijn <joel.bolwijn@gmail.com>
2021-08-11 10:12:09 +0200

    LogIn - Register Centrats

--- lib/features/login/Login.dart
+++ lib/features/login/Login.dart
@@ -14,94 +14,93 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 80.0),
-              child: Container(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
                   width: 180,
                   height: 135,
                   child: Image.asset('assets/images/mamba-logo.jpg')),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                      borderRadius: BorderRadius.circular(13.0),
+              Padding(
+                  padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                        borderRadius: BorderRadius.circular(13.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                        borderRadius: BorderRadius.circular(13.0),
+                      ),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Color(0xFF200758)),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                      borderRadius: BorderRadius.circular(13.0),
+                  )
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                        borderRadius: BorderRadius.circular(13.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                        borderRadius: BorderRadius.circular(13.0),
+                      ),
+                      labelText: 'Contraseña',
+                      labelStyle: TextStyle(color: Color(0xFF200758)),
                     ),
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Color(0xFF200758)),
-                ),
-              )
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                    borderRadius: BorderRadius.circular(13.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                    borderRadius: BorderRadius.circular(13.0),
-                  ),
-                  labelText: 'Contraseña',
-                  labelStyle: TextStyle(color: Color(0xFF200758)),
-                ),
-              )
-            ),
-            TextButton(
-              onPressed: (){
-                //TODO: FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: Text(
-                'Has olvidado tu contraseña?',
-                style: TextStyle(color: Colors.white, fontSize: 17),
+                  )
               ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Color(0xFF200758), borderRadius: BorderRadius.circular(20)
-              ),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context, CupertinoPageRoute(builder: (_) => HomePage()));
+              TextButton(
+                onPressed: (){
+                  //TODO: FORGOT PASSWORD SCREEN GOES HERE
                 },
                 child: Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  'Has olvidado tu contraseña?',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
                 ),
               ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 16.0),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Color(0xFF200758), borderRadius: BorderRadius.circular(20)
+                ),
                 child: TextButton(
                   onPressed: () {
                     Navigator.push(
-                      context, CupertinoPageRoute(builder: (_) => Register()));
+                        context, CupertinoPageRoute(builder: (_) => HomePage()));
                   },
                   child: Text(
-                    'Nuevo usuario? Crea tu cuenta',
-                    style: TextStyle(color: Colors.white, fontSize: 17),
-                  )
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
                 ),
-            ),
-          ],
-        ),
-      )
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 16.0),
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context, CupertinoPageRoute(builder: (_) => Register()));
+                    },
+                    child: Text(
+                      'Nuevo usuario? Crea tu cuenta',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    )
+                ),
+              ),
+            ],
+          ),
+          ),
+      ),
     );
   }
 }
--- lib/features/login/Register.dart
+++ lib/features/login/Register.dart
@@ -36,138 +36,137 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: backgroundColor,
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 80.0, bottom: 16.0),
-                child: Container(
-                    width: 180,
-                    height: 135,
-                    child: Image.asset('assets/images/mamba-logo.jpg')),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                    child: Text(
-                      'Cliente',
-                      style: TextStyle(color: textColorClient, fontSize: 22, fontWeight: fontWeightClient),
-                    ),
-                  ),
-                  Container(
-                    child: Transform.scale( scale: 2.0,
-                      child: new Switch(
-                        onChanged: toggleSwitch,
-                        value: isTrainer,
-                        activeColor: Color(0xFF200758),
-                        activeTrackColor: Color(0x8F190763),
-                        inactiveThumbColor: Color(0xFF200758),
-                        inactiveTrackColor: Color(0xA6190763),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                width: 180,
+                height: 135,
+                child: Image.asset('assets/images/mamba-logo.jpg')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Text(
+                        'Cliente',
+                        style: TextStyle(color: textColorClient, fontSize: 22, fontWeight: fontWeightClient),
                       ),
                     ),
-                  ),
-                  Padding(
+                    Container(
+                      child: Transform.scale( scale: 2.0,
+                        child: new Switch(
+                          onChanged: toggleSwitch,
+                          value: isTrainer,
+                          activeColor: Color(0xFF200758),
+                          activeTrackColor: Color(0x8F190763),
+                          inactiveThumbColor: Color(0xFF200758),
+                          inactiveTrackColor: Color(0xA6190763),
+                        ),
+                      ),
+                    ),
+                    Padding(
                       padding: EdgeInsets.only(left: 20.0, right: 20.0),
                       child: Text(
                         'Entrenador',
                         style: TextStyle(color: textColorTrainer, fontSize: 22, fontWeight: fontWeightTrainer),
                       ),
-                  )
-                ],
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
+                    )
+                  ],
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        labelText: 'Nombre Completo',
+                        labelStyle: TextStyle(color: Color(0xFF200758)),
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
+                    )
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Color(0xFF200758)),
                       ),
-                      labelText: 'Nombre Completo',
-                      labelStyle: TextStyle(color: Color(0xFF200758)),
+                    )
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        labelText: 'Contraseña',
+                        labelStyle: TextStyle(color: Color(0xFF200758)),
+                      ),
+                    )
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 16.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        labelText: 'Repite tu contraseña',
+                        labelStyle: TextStyle(color: Color(0xFF200758)),
+                      ),
+                    )
+                ),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF200758), borderRadius: BorderRadius.circular(20)),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (_) => HomePage()));
+                    },
+                    child: Text(
+                      'Registrate',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
                     ),
-                  )
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
-                      ),
-                      labelText: 'Email',
-                      labelStyle: TextStyle(color: Color(0xFF200758)),
-                    ),
-                  )
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
-                      ),
-                      labelText: 'Contraseña',
-                      labelStyle: TextStyle(color: Color(0xFF200758)),
-                    ),
-                  )
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 16.0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF200758), width: 2.5),
-                        borderRadius: BorderRadius.circular(13.0),
-                      ),
-                      labelText: 'Repite tu contraseña',
-                      labelStyle: TextStyle(color: Color(0xFF200758)),
-                    ),
-                  )
-              ),
-              Container(
-                height: 50,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Color(0xFF200758), borderRadius: BorderRadius.circular(20)),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (_) => HomePage()));
-                  },
-                  child: Text(
-                    'Registrate',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        )
+        ),
     );
   }
 }
