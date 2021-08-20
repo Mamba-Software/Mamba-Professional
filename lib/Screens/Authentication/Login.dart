eae447f0ac127962a2780344a783e8dbec83ff67
JoelBolwijn <joel.bolwijn@gmail.com>
2021-08-20 22:25:47 +0200

    Boto enrere solucionat

--- lib/Screens/Authentication/Login.dart
+++ lib/Screens/Authentication/Login.dart
@@ -28,103 +28,132 @@ class _LoginState extends State<Login> {
   String password = '';
   String error = '';
 
+  Future<bool> _onBackPressed() async {
+    return (await showDialog(
+      context: context,
+      builder: (context) => new AlertDialog(
+        title: Center(child: Text('¿Quieres salir de Mamba?', style: redTextStyle)),
+        content: Row (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                child: Text('No', style: purpleTextStyle.copyWith(fontSize: 18, fontWeight: FontWeight.bold),),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ), // button 1
+              TextButton(
+                child: Text('Sí', style: purpleTextStyle.copyWith(fontSize: 18, fontWeight: FontWeight.bold),),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ), // button 2
+            ]
+        )
+      ),
+    )) ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() :Scaffold(
-      backgroundColor: yellowColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.only(top: 16.0),
-                    width: 200,
-                    height: 100,
-                    child: Image.asset(logoExtended)),
-                Padding(
-                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 13.0, bottom: 0.0),
-                    child: TextFormField(
-                      validator: (val) => val!.isEmpty ? 'Escribe tu email' : null,
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      },
-                      decoration: textFromInputDecoration.copyWith(labelText: 'Email')
-                    )
-                ),
-                Padding(
-                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                    child: TextFormField(
-                      validator: (val) => val!.length < 6 ? 'Introduzca una contraseña con 6 caracteres o más' : null,
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      },
-                      obscureText: true,
-                        decoration: textFromInputDecoration.copyWith(labelText: 'Contraseña')
-                    )
-                ),
-                TextButton(
-                  onPressed: (){
-                    //TODO: FORGOT PASSWORD SCREEN GOES HERE
-                  },
-                  child: Text(
-                    'Has olvidado tu contraseña?',
-                    style: whiteTextStyle,
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      color: purpleColor, borderRadius: BorderRadius.circular(20)
-                  ),
-                  child: TextButton(
-                    onPressed: () async {
-                      if(_formKey.currentState!.validate()){
-                        setState(() {
-                          loading = true;
-                          error = '';
-                        });
-                        dynamic result = await _authenticationService.signIn(
-                            email: email,
-                            password: password
-                        );
-                        if (result == null) {
-                          setState(() {
-                            error = 'No encontramos este usuario.\n Porfavor prueba otra vez';
-                            loading = false;
-                          });
-                        }
-                      }
-                    },
-                    child: Text(
-                      'Login',
-                      style: whiteTextStyle.copyWith(fontSize: 28),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 16.0),
-                  child: TextButton(
-                      onPressed: () {
-                        widget.toggleView();
-                      },
-                      child: Text(
-                        'Nuevo usuario? Crea tu cuenta',
-                        style: whiteTextStyle,
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+      child: loading ? Loading() :Scaffold(
+        backgroundColor: yellowColor,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.only(top: 16.0),
+                      width: 200,
+                      height: 100,
+                      child: Image.asset(logoExtended)),
+                  Padding(
+                      padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 13.0, bottom: 0.0),
+                      child: TextFormField(
+                        validator: (val) => val!.isEmpty ? 'Escribe tu email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                        decoration: textFromInputDecoration.copyWith(labelText: 'Email')
                       )
                   ),
-                ),
-                Center(
-                  child: Text(
-                    error,
-                    style: redTextStyle.copyWith(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
+                  Padding(
+                      padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                      child: TextFormField(
+                        validator: (val) => val!.length < 6 ? 'Introduzca una contraseña con 6 caracteres o más' : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                        obscureText: true,
+                          decoration: textFromInputDecoration.copyWith(labelText: 'Contraseña')
+                      )
                   ),
-                ),
-              ],
+                  TextButton(
+                    onPressed: (){
+                      //TODO: FORGOT PASSWORD SCREEN GOES HERE
+                    },
+                    child: Text(
+                      'Has olvidado tu contraseña?',
+                      style: whiteTextStyle,
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: purpleColor, borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: TextButton(
+                      onPressed: () async {
+                        if(_formKey.currentState!.validate()){
+                          setState(() {
+                            loading = true;
+                            error = '';
+                          });
+                          dynamic result = await _authenticationService.signIn(
+                              email: email,
+                              password: password
+                          );
+                          if (result == null) {
+                            setState(() {
+                              error = 'No encontramos este usuario.\n Porfavor prueba otra vez';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                      child: Text(
+                        'Login',
+                        style: whiteTextStyle.copyWith(fontSize: 28),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 16.0),
+                    child: TextButton(
+                        onPressed: () {
+                          widget.toggleView();
+                        },
+                        child: Text(
+                          'Nuevo usuario? Crea tu cuenta',
+                          style: whiteTextStyle,
+                        )
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      error,
+                      style: redTextStyle.copyWith(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/Screens/Authentication/Register.dart
+++ lib/Screens/Authentication/Register.dart
@@ -54,151 +54,159 @@ class _RegisterState extends State<Register> {
   String password1 = '';
   String password2 = '';
 
+  // OnBackPressed
+  Future<bool> _onBackPressed() async {
+    return widget.toggleView() ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() :Scaffold(
-        backgroundColor: yellowColor,
-        body: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                      padding: EdgeInsets.only(top: 16.0),
-                      width: 200,
-                      height: 100,
-                      child: Image.asset(logoExtended)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                        child: Text(
-                          'Cliente',
-                          style: TextStyle(color: textColorClient, fontSize: 22, fontWeight: fontWeightClient),
-                        ),
-                      ),
-                      Container(
-                        child: Transform.scale( scale: 2.0,
-                          child: new Switch(
-                            onChanged: toggleSwitch,
-                            value: isTrainer,
-                            activeColor: purpleColor,
-                            activeTrackColor: purpleLightColor,
-                            inactiveThumbColor: purpleColor,
-                            inactiveTrackColor: purpleLightColor,
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: loading ? Loading() :Scaffold(
+          backgroundColor: yellowColor,
+          body: Center(
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.only(top: 16.0),
+                        width: 200,
+                        height: 100,
+                        child: Image.asset(logoExtended)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                          child: Text(
+                            'Cliente',
+                            style: TextStyle(color: textColorClient, fontSize: 22, fontWeight: fontWeightClient),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                        child: Text(
-                          'Entrenador',
-                          style: TextStyle(color: textColorTrainer, fontSize: 22, fontWeight: fontWeightTrainer),
+                        Container(
+                          child: Transform.scale( scale: 2.0,
+                            child: new Switch(
+                              onChanged: toggleSwitch,
+                              value: isTrainer,
+                              activeColor: purpleColor,
+                              activeTrackColor: purpleLightColor,
+                              inactiveThumbColor: purpleColor,
+                              inactiveTrackColor: purpleLightColor,
+                            ),
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                      child: TextFormField(
-                        validator: (val) => val!.isEmpty ? 'Escribe tu nombre' : null,
-                        onChanged: (val) {
-                          setState(() => name = val);
-                        },
-                        decoration: textFromInputDecoration.copyWith(labelText: 'Nombre completo')
-                      )
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                      child: TextFormField(
-                        validator: (val) => val!.isEmpty ? 'Escribe tu email' : null,
-                        onChanged: (val) {
-                          setState(() => email = val);
-                        },
-                        decoration: textFromInputDecoration.copyWith(labelText: 'Email')
-                      )
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
-                      child: TextFormField(
-                        validator: (val) => val!.length < 6 ? 'Introduzca una contraseña con 6 caracteres o más' : null,
-                        onChanged: (val) {
-                          setState(() => password1 = val);
-                        },
-                        obscureText: true,
-                        decoration: textFromInputDecoration.copyWith(labelText: 'Contraseña')
-                      )
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 16.0),
-                    child: TextFormField(
-                        validator: (val) => val == password1 ? null : 'Contraseñas no coinciden',
-                        onChanged: (val) {
-                          setState(() => password2 = val);
-                        },
-                        obscureText: true,
-                        decoration: textFromInputDecoration.copyWith(labelText: 'Repite tu contraseña')
+                        Padding(
+                          padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                          child: Text(
+                            'Entrenador',
+                            style: TextStyle(color: textColorTrainer, fontSize: 22, fontWeight: fontWeightTrainer),
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                  Container(
-                    height: 50,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        color: purpleColor, borderRadius: BorderRadius.circular(20)),
-                    child: TextButton(
-                      onPressed: () async {
-                        if(_formKey.currentState!.validate()){
-                          setState(() {
-                            loading = true;
-                          });
-                          dynamic result = await _authenticationService.signUp(
-                              email: email,
-                              password: password1,
-                              name: name,
-                              isTrainer: isTrainer,
-                          );
-                          if (result == null) {
-                            setState(() {
-                              error = 'Porfavor introduce un email válido.';
-                              loading = false;
-                            });
-                          }
-                        }
-                      },
-                      child: Text(
-                        'Registrate',
-                        style: whiteTextStyle.copyWith(fontSize: 28),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 16.0),
-                    child: TextButton(
-                        onPressed: () {
-                          widget.toggleView();
-                        },
-                        child: Text(
-                          'Tienes una cuenta? Inicia sesión',
-                          style: whiteTextStyle,
+                    Padding(
+                        padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                        child: TextFormField(
+                          validator: (val) => val!.isEmpty ? 'Escribe tu nombre' : null,
+                          onChanged: (val) {
+                            setState(() => name = val);
+                          },
+                          decoration: textFromInputDecoration.copyWith(labelText: 'Nombre completo')
                         )
                     ),
-                  ),
-                 Center(
-                    child: Text(
-                      error,
-                      style: redTextStyle.copyWith(fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
+                    Padding(
+                        padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                        child: TextFormField(
+                          validator: (val) => val!.isEmpty ? 'Escribe tu email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                          decoration: textFromInputDecoration.copyWith(labelText: 'Email')
+                        )
                     ),
-                  ),
-                ],
+                    Padding(
+                        padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 0),
+                        child: TextFormField(
+                          validator: (val) => val!.length < 6 ? 'Introduzca una contraseña con 6 caracteres o más' : null,
+                          onChanged: (val) {
+                            setState(() => password1 = val);
+                          },
+                          obscureText: true,
+                          decoration: textFromInputDecoration.copyWith(labelText: 'Contraseña')
+                        )
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 16.0, bottom: 16.0),
+                      child: TextFormField(
+                          validator: (val) => val == password1 ? null : 'Contraseñas no coinciden',
+                          onChanged: (val) {
+                            setState(() => password2 = val);
+                          },
+                          obscureText: true,
+                          decoration: textFromInputDecoration.copyWith(labelText: 'Repite tu contraseña')
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: purpleColor, borderRadius: BorderRadius.circular(20)),
+                      child: TextButton(
+                        onPressed: () async {
+                          if(_formKey.currentState!.validate()){
+                            setState(() {
+                              loading = true;
+                            });
+                            dynamic result = await _authenticationService.signUp(
+                                email: email,
+                                password: password1,
+                                name: name,
+                                isTrainer: isTrainer,
+                            );
+                            if (result == null) {
+                              setState(() {
+                                error = 'Porfavor introduce un email válido.';
+                                loading = false;
+                              });
+                            }
+                          }
+                        },
+                        child: Text(
+                          'Registrate',
+                          style: whiteTextStyle.copyWith(fontSize: 28),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 16.0),
+                      child: TextButton(
+                          onPressed: () {
+                            widget.toggleView();
+                          },
+                          child: Text(
+                            'Tienes una cuenta? Inicia sesión',
+                            style: whiteTextStyle,
+                          )
+                      ),
+                    ),
+                   Center(
+                      child: Text(
+                        error,
+                        style: redTextStyle.copyWith(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+              ),
+            ),
+
             ),
           ),
-
-          ),
-        ),
+      ),
     );
   }
 }
