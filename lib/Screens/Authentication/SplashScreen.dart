563e436481761fc9834119814f99f4dd48b351bf
JoelBolwijn <joel.bolwijn@gmail.com>
2021-09-12 21:41:48 +0200

    splash screen same size as the rest

--- lib/Screens/Authentication/SplashScreen.dart
+++ lib/Screens/Authentication/SplashScreen.dart
@@ -79,8 +79,8 @@ class _SplashScreenState extends State<SplashScreen> {
         children: <Widget>[
           Center(
             child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.3,
-              height: MediaQuery.of(context).size.height * 0.15,
+              width: MediaQuery.of(context).size.width * 0.14,
+              height: MediaQuery.of(context).size.height * 0.07,
               child: CircularProgressIndicator(
                 color: Styles.white,
               ),
@@ -88,8 +88,8 @@ class _SplashScreenState extends State<SplashScreen> {
           ),
           Center(
             child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.15,
-              height: MediaQuery.of(context).size.height * 0.15,
+              width: MediaQuery.of(context).size.width * 0.07,
+              height: MediaQuery.of(context).size.height * 0.07,
               child: Image(
                   image: AssetImage(Constants.logoSimple)
               ),
