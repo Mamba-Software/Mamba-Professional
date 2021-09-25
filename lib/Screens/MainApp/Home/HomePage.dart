be9e265033471c96bc7c730836769ce1b6977541
JoelBolwijn <joel.bolwijn@gmail.com>
2021-09-25 11:42:36 +0200

    curved bottom navigation bar

--- lib/Screens/MainApp/Home/HomePage.dart
+++ lib/Screens/MainApp/Home/HomePage.dart
@@ -7,6 +7,7 @@ import 'package:mamba_castelldefels/Globals/GlobalVars.dart';
 import 'package:mamba_castelldefels/Globals/Styles.dart';
 import 'ChildWidget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 // HomePage for the App. Here the user can change between the diferent pages.
 // In this class we can only see the declaration of those pages and the swiping/changing between screens.
@@ -51,6 +52,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final items = <Widget>[
+      Icon(Icons.person, color: Styles.white, size: 40,),
+      Icon(Icons.fitness_center_rounded, color: Styles.white, size: 40,),
+      Icon(Icons.chat, color: Styles.white, size: 40,),
+    ];
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
         value:SystemUiOverlayStyle(
           statusBarColor: Colors.transparent, //i like transaparent :-)
@@ -70,27 +77,14 @@ class _HomePageState extends State<HomePage> {
             automaticallyImplyLeading: false,
           ),
           backgroundColor: Styles.white,
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: _currentIndex,
-            type: BottomNavigationBarType.shifting,
-            iconSize: 35,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Styles.white,),
-                label: AppLocalizations.of(context)!.profileBottomNav,
-                backgroundColor: Styles.mainColor,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.fitness_center_rounded, color: Styles.white,),
-                label: AppLocalizations.of(context)!.brandBottomNav,
-                backgroundColor: Styles.mainColor,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.chat, color: Styles.white,),
-                label: AppLocalizations.of(context)!.chatBottomNav,
-                backgroundColor: Styles.mainColor,
-              ),
-            ],
+          bottomNavigationBar: CurvedNavigationBar(
+            index: _currentIndex,
+            color: Styles.mainColor,
+            backgroundColor: Colors.transparent,
+            animationCurve: Curves.easeInOut,
+            animationDuration: Duration(milliseconds: 200),
+            height: 60,
+            items: items,
             onTap: (index) {
               _currentIndex = index;
               _pageController.animateToPage(
@@ -100,10 +94,7 @@ class _HomePageState extends State<HomePage> {
               );
               setState(() {});
             },
-            selectedItemColor: Styles.white,
-            selectedLabelStyle: Styles.whiteTextStyle.copyWith(fontSize: 15),
-            unselectedItemColor: Styles.white,
-            unselectedLabelStyle: Styles.whiteTextStyle.copyWith(fontSize: 15),
+
           ),
           body: PageView(
             controller: _pageController,
@@ -129,4 +120,28 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+/*
+items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person, color: Styles.white,),
+                label: AppLocalizations.of(context)!.profileBottomNav,
+                backgroundColor: Styles.mainColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.fitness_center_rounded, color: Styles.white,),
+                label: AppLocalizations.of(context)!.brandBottomNav,
+                backgroundColor: Styles.mainColor,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat, color: Styles.white,),
+                label: AppLocalizations.of(context)!.chatBottomNav,
+                backgroundColor: Styles.mainColor,
+              ),
+            ],
+
+            selectedItemColor: Styles.white,
+            selectedLabelStyle: Styles.whiteTextStyle.copyWith(fontSize: 15),
+            unselectedItemColor: Styles.white,
+            unselectedLabelStyle: Styles.whiteTextStyle.copyWith(fontSize: 15),
+ */
 
