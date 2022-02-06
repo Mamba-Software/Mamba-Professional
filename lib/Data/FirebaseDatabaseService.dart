353eb01acb0a974cffeae48b861c88779abc68a1
JoelBolwijn <joel.bolwijn@gmail.com>
2022-02-06 22:37:36 +0100

    working on script to migrate DATA

--- lib/Data/FirebaseDatabaseService.dart
+++ lib/Data/FirebaseDatabaseService.dart
@@ -34,9 +34,7 @@ class FirebaseDatabaseService {
   String brands = isProduction ? 'Brands' : '7777 Brands';
   String events = isProduction ? 'Events' : '7777 Events';
   String locations = isProduction ? 'Locations' : '7777 Locations';
-  String groupOfQuestions = isProduction
-      ? 'GroupOfQuestions'
-      : '7777 GroupOfQuestions';
+  String groupOfQuestions = isProduction ? 'GroupOfQuestions' : '7777 GroupOfQuestions';
   String questions = isProduction ? 'Questions' : '7777 Questions';
   String answers = isProduction ? 'Answers' : '7777 Answers';
   String conversations = isProduction ? 'Conversations' : '7777 Conversations';
