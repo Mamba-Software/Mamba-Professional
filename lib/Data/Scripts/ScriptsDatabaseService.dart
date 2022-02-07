1294f5a993e8628c696db240f1dc333da46500a7
JoelBolwijn <joel.bolwijn@gmail.com>
2022-02-07 22:10:58 +0100

    migration working for test users in prod database :D

--- lib/Data/Scripts/ScriptsDatabaseService.dart
+++ lib/Data/Scripts/ScriptsDatabaseService.dart
@@ -89,7 +89,7 @@ class ScriptsDatabaseService {
         print('Adding "Notifications" subcollection');
         print('-----------------------------\n');
         QuerySnapshot querySnapshotNotif = await _firestore.collection(notifications).where("userId", isEqualTo: user.id!).get();
-        for (var i=1; i<querySnapshotNotif.docs.length;i++) {
+        for (var i=0; i<querySnapshotNotif.docs.length;i++) {
           String notifId = querySnapshotNotif.docs[i].id;
           print('Notification with ID : '+notifId);
           DocumentSnapshot _documentSnapshot = querySnapshotNotif.docs[i];
@@ -112,16 +112,69 @@ class ScriptsDatabaseService {
         print('Adding "Errors" subcollection');
         print('-----------------------------\n');
         QuerySnapshot querySnapshotErrors = await _firestore.collection(errors).where("userID", isEqualTo: user.id!).get();
-        for (var i=1; i<querySnapshotErrors.docs.length;i++) {
+        for (var i=0; i<querySnapshotErrors.docs.length;i++) {
           String errorId = querySnapshotErrors.docs[i].id;
           print('Error with ID : '+errorId);
-          DocumentSnapshot _documentSnapshot = querySnapshotErrors.docs[i];
+          final DateTime now = DateTime.now();
+          final DateFormat formatter = DateFormat('dd-MM-yyyy');
+          final String formatted = formatter.format(now);
           await _firestore.collection(users).doc(user.id!).collection("Errors").doc(errorId)
               .set({
-                "dateSent": _documentSnapshot.get("dateSent"),
+                "dateSent": formatted,
               });
         }
         print('All Errors Added');
+        print('\n');
+        print('Adding "Brands" subcollection');
+        print('-----------------------------\n');
+        if (user.brandID != null) {
+          DocumentSnapshot _document = await _firestore.collection(brands).doc(user.brandID).get();
+          Brand brand = Brand.fromObjectAllData(_document.id, _document);
+          print('Brand with ID : '+brand.id!);
+          final DateTime now = DateTime.now();
+          final DateFormat formatter = DateFormat('dd-MM-yyyy');
+          final String formatted = formatter.format(now);
+          await _firestore.collection(users).doc(user.id!).collection("Brands").doc(brand.id)
+            .set({
+              "name": brand.name,
+              "logoUrl": brand.logoUrl,
+              "dateJoined": formatted,
+              "myMonthlySessions": 0,
+              "myTotalSessions": 0,
+            });
+        } else {
+          print('Not in Brand');
+        }
+        print('All Brands Added');
+        print('\n');
+        print('Adding "Events" subcollection');
+        print('-----------------------------\n');
+        QuerySnapshot querySnapshotEvents;
+        if (user.isTrainer!) {
+          querySnapshotEvents = await _firestore.collection(events).where("selectedTrainers", arrayContains: user.id!).get();
+        } else {
+          querySnapshotEvents = await _firestore.collection(events).where("joinedMembers", arrayContains: user.id!).get();
+        }
+        for (var i=0; i<querySnapshotEvents.docs.length;i++) {
+          String eventId = querySnapshotEvents.docs[i].id;
+          DocumentSnapshot _documentSnapshot = querySnapshotEvents.docs[i];
+          print('Event with ID : '+eventId);
+          int numClients = _documentSnapshot.get("joinedMembers").length;
+          int numTrainers = _documentSnapshot.get("selectedTrainers").length;
+          await _firestore.collection(users).doc(user.id!).collection("Events").doc(eventId).set({
+            "title": _documentSnapshot.get("title"),
+            "year": _documentSnapshot.get("year"),
+            "month": _documentSnapshot.get("month"),
+            "day": _documentSnapshot.get("day"),
+            "hour": _documentSnapshot.get("hour"),
+            "minute": _documentSnapshot.get("minute"),
+            "duration": _documentSnapshot.get("duration"),
+            "numTrainers": numTrainers,
+            "numClients": numClients,
+            "maxMembers": _documentSnapshot.get("maxMembers"),
+          });
+        }
+        print('All Events Added');
         print('\n');
         print('=================================================================================');
         print('=================================================================================');
