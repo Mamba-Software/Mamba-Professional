0589d956f3f3febc48553bb8d55ed61c87308d91
JoelBolwijn <joel.bolwijn@gmail.com>
2022-03-14 17:53:22 +0100

    wip crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -16,14 +18,8 @@ import 'package:mamba_castelldefels/Globals/Providers/LanguageProvider.dart';
 import 'package:mamba_castelldefels/Globals/GlobalVars.dart';
 import 'package:resize/resize.dart';
 
-import 'Globals/Utils/DynamicLinks/DynamicLinkUtils.dart';
-
 // Declaring Instance of AppThemes();
 AppThemes _appThemes = AppThemes();
-
-//Dynamic Link Utils for retrieving the dynamic link
-var _dynamicLinkUtils = new DynamicLinkUtils();
-
 
 // BackGroundNotificationHandler
 Future<void> _backgroundMessageHandler(RemoteMessage message) async {
@@ -33,33 +29,51 @@ Future<void> _backgroundMessageHandler(RemoteMessage message) async {
 // Starting app function. After initialization, we define the global providers:
 // - Language Provider: To change the Language of the App.
 void main() async {
-  // Initialize App
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  // Firebase Messaging Back Ground Message Handler
-  FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
-  //Get dynamic links on open app
-  PendingDynamicLinkData? initialLink;
-  try {
-    initialLink = await FirebaseDynamicLinks.instance.getInitialLink();
-    if(initialLink != null) brandPath = initialLink.link;
-  } catch (e) {
-    initialLink = null;
-  }
-  // Run App
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<LanguageProvider>(
-            create: (_) => LanguageProvider()
-        ),
-        ChangeNotifierProvider<ThemeProvider>(
-            create: (_) => ThemeProvider()
-        ),
-      ],
-      child: Mamba(),
-    )
-  );
+  await runZonedGuarded(() async {
+    // Initialize App
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    // Firebase Messaging Back Ground Message Handler
+    FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
+    //Get dynamic links on open app
+    PendingDynamicLinkData? initialLink;
+    try {
+      initialLink = await FirebaseDynamicLinks.instance.getInitialLink();
+      if(initialLink != null) brandPath = initialLink.link;
+    } catch (e) {
+      initialLink = null;
+    }
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    // Run App
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<LanguageProvider>(
+              create: (_) => LanguageProvider()
+          ),
+          ChangeNotifierProvider<ThemeProvider>(
+              create: (_) => ThemeProvider()
+          ),
+        ],
+        child: Mamba(),
+      )
+    );
+    _testAsyncErrorOnInit();
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
+
+}
+
+Future<void> _testAsyncErrorOnInit() async {
+  Future<void>.delayed(const Duration(seconds: 2), () async {
+    try {
+      final List<int> list = <int>[];
+      print(list[100]);
+    } catch (error, stackTrace) {
+      await FirebaseCrashlytics.instance.recordError(error, stackTrace, reason: 'as an example of non-fatal error');
+    }
+  });
 }
 
 // Launching the Splash Screen
