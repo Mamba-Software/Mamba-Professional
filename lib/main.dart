af98f749d8d0bc2d70f1fbc0a81e03535dda146c
JoelBolwijn <joel.bolwijn@gmail.com>
2022-03-21 13:21:12 +0100

    test rescale

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ import 'package:mamba_castelldefels/Screens/Authentication/SplashScreen.dart';
 import 'package:mamba_castelldefels/Globals/Providers/LanguageProvider.dart';
 import 'package:mamba_castelldefels/Globals/GlobalVars.dart';
 import 'package:resize/resize.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 // Declaring Instance of AppThemes();
 AppThemes _appThemes = AppThemes();
@@ -110,6 +111,20 @@ class Mamba extends StatelessWidget {
                   GlobalWidgetsLocalizations.delegate,
                   GlobalCupertinoLocalizations.delegate,
                 ],
+                builder: (context, widget) => ResponsiveWrapper.builder(
+                  BouncingScrollWrapper.builder(context, widget!),
+                  maxWidth: 1200,
+                  minWidth: 450,
+                  defaultScale: true,
+                  breakpoints: [
+                    const ResponsiveBreakpoint.resize(450, name: MOBILE),
+                    const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                    const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                    const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+                    const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+                  ],
+                  background: Container(color: const Color(0xFFF5F5F5))
+                ),
                 home: SplashScreen(),
                 routes: {
                   "SplashScreen": (_) => SplashScreen(),
