b45f52992921d724ee5fc89a3ff872639f9dacdf
JoelBolwijn <joel.bolwijn@gmail.com>
2022-04-26 08:51:27 +0200

    MAMBA VERSION 3.0.8+11 - 26/04/2022

--- lib/Screens/Admin/Admin.dart
+++ lib/Screens/Admin/Admin.dart
@@ -121,8 +121,8 @@ class EachList extends StatelessWidget{
         );
         break;
       case 3:
-        var result = await _script.migrateEventsDataMarch24th();
-        print("RESULT: "+result.toString());
+        //var result = await _script.migrateEventsDataMarch24th();
+        //print("RESULT: "+result.toString());
         break;
     }
 
