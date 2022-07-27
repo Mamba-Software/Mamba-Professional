07b669e49e4901d35cbd192285e3a73b2fb84217
JoelBolwijn <joel.bolwijn@gmail.com>
2022-07-27 16:43:46 +0200

    MAMBA VERSION 3.0.9+12 - 27/07/2022

--- lib/Globals/GlobalVars.dart
+++ lib/Globals/GlobalVars.dart
@@ -5,7 +5,7 @@ import 'package:mamba_castelldefels/Data/Models/Brand.dart';
 import 'package:mamba_castelldefels/Data/Models/Usuario.dart';
 
 // IS PRODUCTION?
-bool isProduction = false;
+bool isProduction = true;
 
 // App Version
 var appVersion = "MAMBA v0.0.9";
