fa56603be554fa72bda43d81656767e49cb95eec
joelbolwijnpinell <joel.bolwijn@gmail.com>
2022-08-01 12:00:34 +0200

    MAMBA VERSION 3.0.10+13 - 01/08/2022

--- lib/Globals/GlobalVars.dart
+++ lib/Globals/GlobalVars.dart
@@ -8,7 +8,7 @@ import 'package:mamba_castelldefels/Data/Models/Usuario.dart';
 bool isProduction = true;
 
 // App Version
-var appVersion = "MAMBA v0.0.9";
+var appVersion = "MAMBA v0.0.10";
 
 // App
 var androidGooglePlayUrl = "https://play.google.com/store/apps/details?id=com.mamba.mambastyleapp";
