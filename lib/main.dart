14a6ba3349fb1830fe0817683acee9390dd26f2e
joelbolwijnpinell <joel.bolwijn@gmail.com>
2022-08-23 20:12:26 +0200

    Bonos backend on work 4.0

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'package:mamba_castelldefels/Globals/Providers/ThemeProvider.dart';
 import 'package:mamba_castelldefels/Globals/Styles/AppThemes/AppThemes.dart';
 import 'package:mamba_castelldefels/Globals/ChatCore/ChatCore.dart';
 import 'package:mamba_castelldefels/Globals/Widgets/GroupOfComponents/Events/EventFeedback.dart';
-import 'package:mamba_castelldefels/Screens/MainApp/MambaPro/02-Que/005-Bonos/BonosRequests.dart';
+import 'package:mamba_castelldefels/Screens/MambaPro/HasBrandScreens/02-Que/005-Bonos/BonosRequests.dart';
 import 'package:provider/provider.dart';
 import 'package:mamba_castelldefels/Globals/Constants.dart';
 import 'package:mamba_castelldefels/Globals/Idiomas/Idiomas.dart';
