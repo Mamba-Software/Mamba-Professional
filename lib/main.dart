51e8f9a0790f92ce73d6b0e8ef50d2248d340f5b
joelbolwijnpinell <joel.bolwijn@gmail.com>
2022-08-31 12:45:38 +0200

    MAMBA PROFESSIONAL VERSION 0.0.1+1 - 31/08/2022 -- 7777 Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
