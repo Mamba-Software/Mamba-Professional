a9adc732e3c2424698c3fc78fca8feda3e7188d6
JoelBolwijn <joel.bolwijn@gmail.com>
2022-09-30 18:30:29 +0200

    MAMBA PROFESSIONAL VERSION 0.0.4+4 - 30/09/2022

--- lib/Globals/GlobalVars.dart
+++ lib/Globals/GlobalVars.dart
@@ -9,10 +9,10 @@ import '../Data/LibraryModels/lColor.dart';
 import '../Data/LibraryModels/lPaymentMethod.dart';
 
 // IS PRODUCTION ?
-bool isProduction = false;
+bool isProduction = true;
 
 // App Version
-var appVersion = "MAMBA v0.0.11";
+var appVersion = "MAMBA v0.0.4";
 
 List<lColor> currentColors = [];
 List<lDegradate> currentDegradates = [];//0x00000000, 0xFFE53935, 0xFF43A047, 0xFF1E88E5, 0xFF8E24AA, 0xFFFB8C00, 0xFFFDD835
