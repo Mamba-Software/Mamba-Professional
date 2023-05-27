12624dda0bf195da448708dc11e8f9c0fce1683d
JoelBolwijn <joel.bolwijn@gmail.com>
2023-05-27 13:08:10 -0400

    MAMBA PROFESSIONAL VERSION 0.0.14+14 - 22/05/2023 - Back to Dev

--- lib/Globals/GlobalVars.dart
+++ lib/Globals/GlobalVars.dart
@@ -18,7 +18,7 @@ bool isProduction = true;
 var appVersion = "MAMBA v0.0.14";
 
 List<lColor> currentColors = [];
-List<lDegradate> currentDegradates = []; //0x00000000, 0xFFE53935, 0xFF43A047, 0xFF1E88E5, 0xFF8E24AA, 0xFFFB8C00, 0xFFFDD835
+List<lDegradate> currentDegradates = [];//0x00000000, 0xFFE53935, 0xFF43A047, 0xFF1E88E5, 0xFF8E24AA, 0xFFFB8C00, 0xFFFDD835
 
 // App
 var androidGooglePlayUrl = "https://play.google.com/store/apps/details?id=com.mamba.mambaprofessionalapp";
