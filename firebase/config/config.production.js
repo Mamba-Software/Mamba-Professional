26d020aaa36d58f14cd42d2d484984e2c48049b3
JoelBolwijn <joel.bolwijn@gmail.com>
2023-12-15 17:18:04 +0100

    wip firebase configuration

---
+++ firebase/config/config.production.js
@@ -0,0 +1,4 @@
+module.exports = {    
+    storageBucket: "mamba-style.appspot.com",
+    sendGridApiKey: "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"
+};
---
+++ firebase/config/config.staging.js
@@ -0,0 +1,4 @@
+module.exports = {    
+    storageBucket: "mamba-fitness-test.appspot.com",
+    sendGridApiKey: "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"
+};
