92b6c6f1e02608fe1552f04c18c65ed3e2a0b28a
jmfinestres <jmfinestres@revertis.com>
2024-02-26 19:31:13 +0100

    Changes

--- lib/Globals/Constants.dart
+++ lib/Globals/Constants.dart
@@ -1,5 +1,6 @@
 // Constants that are final and will NEVER change.
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants {
   // App Name
@@ -89,8 +90,9 @@ class Constants {
       .where('isAdmin', isEqualTo: false);
   static get brandQuery => FirebaseFirestore.instance.collection('Brands');
   static get bonosCollection => 'TestBonos';
-  static get baseUrl =>
-      'https://europe-west1-mamba-fitness-dev.cloudfunctions.net/stripeApi';
+  static get baseUrl => dotenv.env['URLSTRIPE'];
+  /*static get baseUrl =>
+      'https://europe-west1-mamba-fitness-dev.cloudfunctions.net/stripeApi';*/
 
   static get merchantDisplayName => 'Mamba Software SL';
 }
