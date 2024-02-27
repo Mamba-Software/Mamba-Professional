8c9a5544c1e0b70be274363e62185d11b6eb129f
jmfinestres <jmfinestres@revertis.com>
2024-02-27 17:30:04 +0100

    Prod changes

--- lib/Stripe/Data/data_repository/stripe_connect_data_provider.dart
+++ lib/Stripe/Data/data_repository/stripe_connect_data_provider.dart
@@ -12,62 +12,6 @@ class StripeConnectDataProvider {
     return result;
   }
 
-  static Future createStripeAccount(Brand brand) async {
-    BrandDataService _brandDataService = BrandDataService();
-    String apiKey =
-        "sk_test_51OWbhYIhy0dvY0FfOinR0wbP0UoVeu2WEr1ovTTw1crqSqNy51tza6koJuVUwkg8JdLYLStZIwCXKwwKQ9nmarSp00ykqCvB62";
-    String url = 'https://api.stripe.com/v1/accounts';
-
-    final int unixTimestamp =
-        (DateTime.now().millisecondsSinceEpoch / 1000).round();
-    String ipAddress =
-        '192.168.20.20'; // This should be dynamically obtained or set as per your requirement
-
-    final response = await http.post(
-      Uri.parse(url),
-      headers: {
-        'Authorization': 'Bearer $apiKey',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: {
-        'type': 'custom',
-        'country': 'ES',
-        'individual[first_name]':
-            brand.name, // Assuming brand.userName exists and is appropriate
-        'individual[last_name]': brand.name,
-        'individual[dob][day]': '1',
-        'individual[dob][month]': '1',
-        'individual[dob][year]': '1990',
-        'individual[phone]': '+34666777888',
-        'individual[id_number]': '000000000',
-        'individual[address][country]': 'ES',
-        'individual[address][state]': 'Barcelona',
-        'individual[address][city]': 'Barcelona',
-        'individual[address][line1]': 'Carrer de la Diputació, 238',
-        'individual[address][postal_code]': '08007',
-        'tos_acceptance[date]': unixTimestamp.toString(),
-        'tos_acceptance[ip]': ipAddress,
-        'business_type': 'individual',
-        'business_profile[mcc]': '8999',
-        'business_profile[url]': 'https://techanion.com',
-        'metadata[user_id]':
-            brand.id, // Assuming brand.id exists and is appropriate
-        'metadata[user_name]': brand.name,
-        'capabilities[card_payments][requested]': 'true',
-        'capabilities[transfers][requested]': 'true',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      _brandDataService.updateBrandStripe(brand.id!, false, data['id']);
-      print("Account created: ${data['id']}");
-    } else {
-      print("Failed to create account: ${response.body}");
-    }
-    return response;
-  }
-
   static Future cancelSubscription({required String subscriptionId}) async {
     //TODO CANCEL SUB DESDE PRO
     /*
