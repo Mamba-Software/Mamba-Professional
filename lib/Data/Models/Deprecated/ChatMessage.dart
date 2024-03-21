4fd514d27adedae20c0c2a64357c2cdb5460cc72
JoelBolwijn <joel.bolwijn@gmail.com>
2024-03-21 19:45:51 +0100

    wip

--- lib/Data/Models/Deprecated/ChatMessage.dart
+++
@@ -1,7 +0,0 @@
-
-class ChatMessage{
-  String? messageContent;
-  String? messageType;
-  String? time;
-  ChatMessage({required this.messageContent, required this.messageType, required this.time});
-}
