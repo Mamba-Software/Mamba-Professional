6040077197705b94940481bf366ec40ef630bf4c
JoelBolwijn <joel.bolwijn@gmail.com>
2024-05-31 10:14:43 +0200

    wip create event button

--- lib/calendar/widgets/calendar_action_button.dart
+++ lib/calendar/widgets/calendar_action_button.dart
@@ -1,181 +1,150 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:graphic/graphic.dart';
+import 'package:mamba/calendar/widgets/zoom_display.dart';
 import 'package:mamba/commons/constants/constants.dart';
 import 'package:mamba/commons/extensions/context.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class CalendarActionButton extends StatefulWidget {
+class CalendarActionButton extends StatelessWidget {
+  final double timeSlotViewScale;
+  final CalendarView calendarView;
   final Function(bool) onCreateEventTap;
 
   const CalendarActionButton({
+    required this.timeSlotViewScale,
+    required this.calendarView,
     required this.onCreateEventTap,
   });
 
   @override
-  _CalendarActionButtonState createState() => _CalendarActionButtonState();
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        if (context.isMobile || context.isTablet) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              CalendarSpeedDial(                
+                withLabel: false,
+                isDialOpen: ValueNotifier(false),
+                onCreateEventTap: onCreateEventTap,                
+              ),
+              const SizedBox(height: 4),
+              ZoomDisplay(
+                timeSlotViewScale: timeSlotViewScale,
+                calendarView: calendarView,
+              ),
+            ],
+          );
+        } else {
+          return CalendarSpeedDial(
+            withLabel: true,
+            isDialOpen: ValueNotifier(false),
+            onCreateEventTap: onCreateEventTap,
+          );
+        }
+      },
+    );
+  }
 }
 
-class _CalendarActionButtonState extends State<CalendarActionButton> {
-  // Dial Open
-  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
+class CalendarSpeedDial extends StatelessWidget {
+  final Function(bool) onCreateEventTap;
+  final ValueNotifier<bool> isDialOpen;
+  final bool withLabel;
+
+  const CalendarSpeedDial({
+    required this.onCreateEventTap,
+    required this.isDialOpen,
+    required this.withLabel,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Builder(builder: (context) {
-      if (context.isMobile || context.isTablet) {
-        return SpeedDial(
-          heroTag: "46",
-          animatedIcon: AnimatedIcons.add_event,
-          animationDuration: Duration(milliseconds: animationDefaultDuration),
-          foregroundColor: context.colorScheme.onSecondary,
-          overlayColor: context.theme.scaffoldBackgroundColor,
-          overlayOpacity: 0.95,
-          spacing: defaultPadding,
-          spaceBetweenChildren: defaultPadding,
-          openCloseDial: isDialOpen,
+    return SpeedDial(
+      heroTag: "46",
+      animatedIcon: AnimatedIcons.add_event,
+      animationDuration: Duration(milliseconds: animationDefaultDuration),
+      foregroundColor: context.colorScheme.onSecondary,
+      backgroundColor: context.colorScheme.secondary,
+      overlayColor: context.theme.scaffoldBackgroundColor,
+      overlayOpacity: 0.95,
+      spacing: defaultPadding,
+      spaceBetweenChildren: defaultPadding,
+      openCloseDial: isDialOpen,
+      label: withLabel
+          ? Text(
+              context.l10n.createEvent,
+              style: context.textTheme.bodyLarge?.copyWith(
+                fontSize: title1,
+                color: context.colorScheme.onSecondary,
+              ),
+              textAlign: TextAlign.right,
+            )
+          : null,
+      children: [
+        SpeedDialChildWidget(
+          icon: Icons.groups,
+          labelText: context.l10n.groupEvent,
+          labelDesc: context.l10n.groupEventDesc,
+          onTap: () => onCreateEventTap(false),
+        ).build(context),
+        SpeedDialChildWidget(
+          icon: Icons.person,
+          labelText: context.l10n.privateEvent,
+          labelDesc: context.l10n.privateEventDesc,
+          onTap: () => onCreateEventTap(true),
+        ).build(context),
+      ],
+    );
+  }
+}
+
+class SpeedDialChildWidget {
+  final IconData icon;
+  final String labelText;
+  final String labelDesc;
+  final VoidCallback onTap;
+
+  const SpeedDialChildWidget({
+    required this.icon,
+    required this.labelText,
+    required this.labelDesc,
+    required this.onTap,
+  });
+
+  SpeedDialChild build(BuildContext context) {
+    return SpeedDialChild(
+      elevation: 4,
+      backgroundColor: context.colorScheme.background,
+      shape: const CircleBorder(),
+      child: Icon(
+        icon,
+        size: iconSize,
+        color: context.colorScheme.onBackground,
+      ),
+      labelWidget: Padding(
+        padding: EdgeInsets.symmetric(horizontal: defaultPaddingSmall),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            SpeedDialChild(
-              elevation: 4,
-              backgroundColor: context.colorScheme.background,
-              shape: const CircleBorder(),
-              child: Icon(
-                Icons.groups,
-                size: iconSize,
-                color: context.colorScheme.onBackground,
-              ),
-              labelWidget: Padding(
-                padding: EdgeInsets.symmetric(horizontal: defaultPaddingSmall),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      context.l10n.groupEvent,
-                      style: context.textTheme.titleLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                    Text(
-                      context.l10n.groupEventDesc,
-                      style: context.textTheme.bodyLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                  ],
-                ),
-              ),
-              onTap: () => widget.onCreateEventTap(false),
+            Text(
+              labelText,
+              style: context.textTheme.titleLarge,
+              textAlign: TextAlign.right,
             ),
-            SpeedDialChild(
-              elevation: 4,
-              backgroundColor: context.colorScheme.background,
-              shape: const CircleBorder(),
-              child: Icon(
-                Icons.person,
-                size: iconSize,
-                color: context.colorScheme.onBackground,
-              ),
-              labelWidget: Padding(
-                padding: EdgeInsets.symmetric(horizontal: defaultPaddingSmall),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      context.l10n.privateEvent,
-                      style: context.textTheme.titleLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                    Text(
-                      context.l10n.privateEventDesc,
-                      style: context.textTheme.bodyLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                  ],
-                ),
-              ),
-              onTap: () => widget.onCreateEventTap(true),
+            Text(
+              labelDesc,
+              style: context.textTheme.bodyLarge,
+              textAlign: TextAlign.right,
             ),
           ],
-        );
-      } else {
-        return SpeedDial(
-          heroTag: "46",
-          label: Text(
-            context.l10n.createEvent,
-            style: context.textTheme.titleLarge?.copyWith(color: context.colorScheme.onSecondary),
-            textAlign: TextAlign.right,
-          ),
-          animatedIcon: AnimatedIcons.add_event,
-          animationDuration: Duration(milliseconds: animationDefaultDuration),
-          foregroundColor: context.colorScheme.onSecondary,
-          overlayColor: context.theme.scaffoldBackgroundColor,
-          overlayOpacity: 0.95,
-          spacing: defaultPadding,
-          spaceBetweenChildren: defaultPadding,
-          openCloseDial: isDialOpen,          
-          children: [
-            SpeedDialChild(
-              elevation: 4,
-              backgroundColor: context.colorScheme.background,
-              shape: const CircleBorder(),
-              child: Icon(
-                Icons.groups,
-                size: iconSize,
-                color: context.colorScheme.onBackground,
-              ),
-              labelWidget: Padding(
-                padding: EdgeInsets.symmetric(horizontal: defaultPaddingSmall),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      context.l10n.groupEvent,
-                      style: context.textTheme.titleLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                    Text(
-                      context.l10n.groupEventDesc,
-                      style: context.textTheme.bodyLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                  ],
-                ),
-              ),
-              onTap: () => widget.onCreateEventTap(false),
-            ),
-            SpeedDialChild(
-              elevation: 4,
-              backgroundColor: context.colorScheme.background,
-              shape: const CircleBorder(),
-              child: Icon(
-                Icons.person,
-                size: iconSize,
-                color: context.colorScheme.onBackground,
-              ),
-              labelWidget: Padding(
-                padding: EdgeInsets.symmetric(horizontal: defaultPaddingSmall),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      context.l10n.privateEvent,
-                      style: context.textTheme.titleLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                    Text(
-                      context.l10n.privateEventDesc,
-                      style: context.textTheme.bodyLarge,
-                      textAlign: TextAlign.right,
-                    ),
-                  ],
-                ),
-              ),
-              onTap: () => widget.onCreateEventTap(true),
-            ),
-          ],
-        );
-      }
-    });
+        ),
+      ),
+      onTap: onTap,
+    );
   }
 }
